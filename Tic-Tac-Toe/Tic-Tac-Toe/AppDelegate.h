6b9204521fbacd8d888f795133e90df9b8564d51
Cordavi <michaelamundsen@gmail.com>
2016-03-18 17:34:14 -0400

    Commit inital xc project.

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tic-Tac-Toe
+//
+//  Created by Cordavi on 3/18/16.
+//  Copyright © 2016 Cordavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tic-Tac-Toe
+//
+//  Created by Cordavi on 3/18/16.
+//  Copyright © 2016 Cordavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
