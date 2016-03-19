994e9080c883fd2be041e34ce5ead777f147a383
Dare <dryan@scrippsnetworks.com>
2016-03-18 22:25:00 -0400

    Add selection logic to buttons

--- Tic-Tac-Toe/Tic-Tac-Toe/ViewController.m
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.m
@@ -9,14 +9,37 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttons;
+@property (nonatomic) BOOL isPlayerOneTurn;
 @end
 
 @implementation ViewController
 
 -(void)viewDidLoad {
   [super viewDidLoad];
-
+  
 }
+
+- (void)setupGame {
+  self.isPlayerOneTurn = NO;
+}
+
+- (void)makeMoveForSpace:(NSUInteger) place {
+  NSString *moveString = self.isPlayerOneTurn ? @"X" : @"O";
+  for (UIButton *button in self.buttons) {
+    if (button.tag == place) {
+      [button setTitle:moveString forState:UIControlStateNormal];
+    }
+  }
+  self.isPlayerOneTurn = !self.isPlayerOneTurn;
+}
+
+-(IBAction)buttonTapped:(id)sender {
+  NSInteger buttonTag = ((UIButton *)sender).tag;
+  UIButton *button = sender;
+  [self makeMoveForSpace:buttonTag];
+}
+
+
 
 @end
